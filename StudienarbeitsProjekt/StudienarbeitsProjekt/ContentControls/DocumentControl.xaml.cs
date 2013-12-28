b5bedbe9b2189cdc66f53001a439f48007e8d6a8
FSchiefer <anders33@web.de>
2013-12-28 11:05:16 +0100

    Document

---
+++ StudienarbeitsProjekt/StudienarbeitsProjekt/ContentControls/DocumentControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Surface.Presentation.Controls;
+using System.Windows.Xps.Packaging;
+using System.IO;
+
+namespace StudienarbeitsProjekt.ContentControls
+{
+    /// <summary>
+    /// Interaktionslogik für DocumentControl.xaml
+    /// </summary>
+    public partial class DocumentControl : ScatterViewItem
+    {
+        public DocumentControl()
+        {
+
+            InitializeComponent();
+
+            XpsDocument xpsDoc = new XpsDocument(@"C:\Studiengaenge\test.xps" ,
+                                       FileAccess.Read);
+            
+            myDocViewer.Document = xpsDoc.GetFixedDocumentSequence();
+            test.Width = myDocViewer.Width;
+            test.Height = myDocViewer.Height;
+            xpsDoc.Close();
+        }
+    }
+}
